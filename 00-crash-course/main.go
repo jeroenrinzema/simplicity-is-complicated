0acdda0577ea7f729d782bd5d9d1e4f62b9813a1
Jeroen Rinzema <post@jeroen.no>
2019-11-14 18:23:21 +0100

    feat: improved crash course example

--- 00-crash-course/main.go
+++ 00-crash-course/main.go
@@ -2,14 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
-	// variables are strict typed
-	name := "Jeroen"
-
-	fmt.Println(swap(name, "hello"))
-}
-
 // functions could return one or more arguments
 func swap(x string, y string) (string, string) {
 	return y, x
+}
+
+func main() {
+	// GO has common types such as strings, ints and more
+	// variables are strict typed
+	name := "Jeroen"
+
+	start, end := swap(name, "hello")
+	fmt.Println(start, end)
+
+	// Slices (Arrays) could be buffered and unbuffered
+	list := [2]string{"Gopher", "Golang"}
+	fmt.Println(list)
+
+	// maps (also known as dictionaries) hold key/values
+	dict := make(map[string]string)
+	dict["world"] = "the earth, with all its countries, peoples and natural features"
+	dict["computer"] = "an electronic machine that can store, organize and find information, do calculations and control other machines"
+
+	fmt.Println(dict["world"])
+
+	// you could loop over various types
+	for key := range dict {
+		fmt.Println(key)
+	}
+
+	// Loops could run endlessly
+	for {
+		fmt.Println("I am not a while loop.")
+		break
+	}
 }
--- 40-goroutines/20-simple-channels.go
+++ 40-goroutines/20-simple-channels.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func health(status chan bool) {
+	status <- true
+}
+
+func main() {
+	status := make(chan bool)
+	// remember unbuffered channels could cause a deadlock if health is not ran in a seperate go routine
+	go health(status)
+
+	res := <-status
+	fmt.Println(res)
+}
